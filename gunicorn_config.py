9faf3e61e3b31c6aa59b269b4da1dec567e78115
Joe-Costa <jcostaasc@me.com>
2024-04-04 10:19:14 -0700

    Removed gunicorn conf

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-bind = os.environ.get('GUNICORN_BIND', '127.0.0.1:5000')
-forwarded_allow_ips = '*'
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
